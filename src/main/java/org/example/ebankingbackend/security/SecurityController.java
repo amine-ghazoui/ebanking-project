9b36b5dca12f5f4667c3929e523cf12235e84529
amine-ghazoui <ghazouimohamedamine@gmail.com>
2025-05-06 13:39:17 +0100

    "Ajout de la configuration Spring Security avec authentification JWT pour sécuriser les endpoints"

---
+++ src/main/java/org/example/ebankingbackend/security/SecurityController.java
@@ -0,0 +1,16 @@
+package org.example.ebankingbackend.security;
+
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class SecurityController {
+
+    @GetMapping("/profile")
+    public Authentication authentication(Authentication authentication) {
+        return authentication;
+    }
+}
